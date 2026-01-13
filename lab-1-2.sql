d1df277013af99aa0a9b322bcc0ac9e907d8e5bb
SomilSanghvi <somil.sanghvi@kellogg.northwestern.edu>
2026-01-13 16:57:56 +0000

    Complete lab work

--- lab-1-2.sql
+++ lab-1-2.sql
@@ -14,4 +14,6 @@
 -- | 2001 | Seattle Mariners                  | 116  |
 -- +------+-----------------------------------+------+
 
-
+.mode columns
+.headers on
+SELECT year,name,wins FROM teams ORDER BY (wins);

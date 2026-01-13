d5918df30893ba2d8c3a578b41386010a1553923
SomilSanghvi <somil.sanghvi@kellogg.northwestern.edu>
2026-01-13 17:33:38 +0000

    added code

---
+++ school-5-insert.sql
@@ -0,0 +1,14 @@
+-- INSERT INTO students
+--     ...columns
+--     VALUES
+--         ...values
+
+INSERT INTO students (
+    first_name, 
+    last_name,
+    email
+) VALUES (
+    "Somil",
+    "Sanghvi",
+    "somil.h.sanghvi@gmail.com"
+);
